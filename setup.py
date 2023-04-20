29d33f1af8a0686b8a6d79fbb4c6b1c49f03736a
synset-ai <steps137ai@gmail.com>
2023-04-20 15:44:46 +0300

    <steps137ai@gmail.com>

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="QuNet",
-    version="0.0.1",
+    version="0.0.2",
     author="synset",
     author_email="steps137ai@gmail.com",
     description="Working with deep learning models",
